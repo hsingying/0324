8eaa1cff5abe80a7778b6b2587ae80bbb2e44e73
user <user@MA324-12>
2017-03-24 16:31:32 +0800

    加入專案檔案。

---
+++ 0317/Areas/Order/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0317.Areas.Order.Controllers
+{
+    public class OrderController : Controller
+    { // GET: Order/Order
+        /// <summary>
+        /// 訂單管理系統
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //Models.OrderService orderservice = new Models.OrderService();
+            //var order = orderservice.GetOrderById("111");
+            //ViewBag.CustId = order.CustId;
+            //ViewBag.CustName = order.CustName;
+            ViewBag.Desc = "我是viewbag";
+            ViewData["Desc"] = "我是viewdata";
+            TempData["Desc"] = "我是tempdata";
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            //ViewBag.Desc1 = "我是viewbag";
+            //ViewData["Desc2"] = "我是viewdata";
+            //TempData["Desc3"] = "我是tempdata";
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+            //return RedirectToAction("index");
+      
+        }
+  
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            //var result = new Models.Order();
+            //result.CustId = "GSS";
+            //result.CustName = "叡揚資訊";
+            var result = new Models.Order(){ CustId = "GSS", CustName = "叡揚資訊" };
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ 0317/Areas/Order/Models/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _0317.Areas.Order.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder(Models.Order order)
+        {
+ 
+        }
+        public Models.Order GetOrderById(String id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚";
+            return result;
+        }
+        public List<Models.Order> GetOrderByCondition()
+        {
+            return new List<Order>();
+        }
+        public void DeleteOrderById()
+        {
+
+        }
+        public void UpdateOrderById()
+        {
+
+        }
+    }
+}
